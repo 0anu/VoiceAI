1224c7240b3785f4945636839aeab039afaf8b88
Anuj <gautamanujom786@gmail.com>
2026-01-15 00:21:39 +0530

    Audio Processor Done.

---
+++ app/utils/audio_processor.py
@@ -0,0 +1,18 @@
+from faster_whisper import WhisperModel
+
+class AudioProcessor:
+    
+    def __init__(self, model_size="small", compute_type="int8"):
+        self.model = WhisperModel(model_size, device="cpu")
+
+    def transcribe(self, audio_path):
+        segment_raw, _ = self.model.transcribe(
+            audio_path,
+            task="transcribe",
+            language="en",
+            beam_size=5,
+            best_of=5
+        )
+
+        return "".join([segment.text for segment in segment_raw])
+    
